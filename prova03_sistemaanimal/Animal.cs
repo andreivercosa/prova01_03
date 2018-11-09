04c4069236ec2e5c39d6ef2c18a84479d72028ee
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-09 18:43:14 -0400

    quetao3

---
+++ prova03_sistemaanimal/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace prova03_sistemaanimal
+{
+    public class Animal
+    {
+        private string raca; 
+        private string nome;
+
+        public void pegarRaca(string raca)
+        {
+            this.raca = raca;
+        }
+
+        public void pegarNome(string nome){
+            this.nome = nome;
+        }
+
+        public string devolveRaca(){
+            return raca;
+        }
+
+        public string devolveNome()
+        {
+            return nome;
+        }
+        public Animal()
+        {
+        }
+    }
+}
---
+++ prova03_sistemaanimal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace prova03_sistemaanimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+
+            Console.WriteLine("Informe a Raca do animal");
+            animal.pegarRaca(Console.ReadLine());
+
+            Console.WriteLine("Informe o nome do animal");
+            animal.pegarNome(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("O animal com o nome {0} eh da raca {1}",
+                              animal.devolveNome(), animal.devolveRaca());
+        }
+    }
+}
